503fbf2e7a74e67bb2f209c5ef30935fc57cd588
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-08-11 04:09:36 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const popup = document.querySelector('.popup');
 
+//Main Function: Play Rock, Paper, Scissor Game
+function play(e) {
+ restart.style.display = 'inline-block';
+ 
+}
+
+
+//Event Listeners
+choices.forEach(choice => choice.addEventListener('click', play));
