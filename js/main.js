85af57aaec803d778885147262387b58ab718eeb
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-08-24 00:45:52 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,7 +35,27 @@ function getComputerChoice() {
 
 //Function: Determine Winner
 function getWinner(p,c) {
-
+ if(p === c) {
+  return 'draw';
+ } else if(p === 'rock') {
+  if(c === 'paper') {
+   return 'computer';
+  } else {
+   return 'player';
+  }
+ } else if(p === 'paper') {
+  if (c === 'scissors') {
+   return 'computer';
+  } else {
+   return 'player';
+  }
+ } else if(p === 'scissors') {
+  if (c === 'rock';) {
+   return 'computer';
+  } else {
+   return 'player';
+  }
+ }
 }
 
 //Function: Scoreboard 
